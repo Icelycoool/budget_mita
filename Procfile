dd27ba22127c7562f3e1da654208e0375a7cfe5c
Icelycoool <mohammedcali@proton.me>
2024-09-16 05:10:45 +0300

    Fixing app module not available error

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -b 0.0.0.0:5000 backend.main:app
+web: gunicorn -w 4 -b 0.0.0.0:5000 backend.run:app
--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,7 @@
 from main import create_app
 from config import ProdConfig
 
+app = create_app(ProdConfig)
+
 if __name__=="__main__":
-    app = create_app(ProdConfig)
+    app.run()
